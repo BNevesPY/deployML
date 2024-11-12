0f50511e843d97f1457fab6487bb37641ba271fd
BNevesPY <91921691+BNevesPY@users.noreply.github.com>
2024-11-12 23:43:09 +0100

    Update links.py

--- links.py
+++ links.py
@@ -13,4 +13,8 @@ st.info('Developer')
 
 icon_size = 20
 
+
 st_button('box', 'https://1f736c6c1baf4d6ef1.gradio.live', 'Check box cnnections', icon_size)
+st_button('box', 'https://ce9079fd173a5d7f24.gradio.live', 'Solar Cells Detect', icon_size)
+st_button('box', 'https://8f361a7b6e12208a41.gradio.live', 'Cientifical Judgement', icon_size)
+st_button('box', 'https://2290e135217e0e9fd1.gradio.live', 'TUV Judgement', icon_size)
